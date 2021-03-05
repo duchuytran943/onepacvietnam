8a0b48de3ef06b61cda864554964fe29727dac41
duchuytran943 <duchuytran943@gmail.com>
2021-03-05 09:56:50 +0700

    Remove some code test config

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,56 +1,21 @@
 <template>
   <div class="home">
     <h1>This is home page</h1>
-    <div>
-      <div>
-        <Foo></Foo>
-      </div>
-      <hr />
-      <div>
-        <Bar></Bar>
-      </div>
-    </div>
-    <div>{{ collection }}</div>
   </div>
 </template>
 
 <script>
-import httpAxios from '@/httpAxios';
-import Foo from '@/components/Foo.vue';
-import Bar from '@/components/Bar.vue';
-
 export default {
   name: 'Home',
 
-  components: {
-    Foo,
-    Bar,
-  },
+  components: {},
 
   data() {
-    return {
-      collection: [],
-    };
+    return {};
   },
 
-  created() {
-    this.callApi();
-  },
+  created() {},
 
-  methods: {
-    callApi() {
-      httpAxios({
-        url: `${this.$backendUrl}/search?q=test`,
-        method: 'GET',
-      })
-        .then(response => {
-          console.log('response', response);
-          this.collection = response;
-        })
-        .catch(error => {
-          console.log('error', error);
-        });
-    },
-  },
+  methods: {},
 };
 </script>
